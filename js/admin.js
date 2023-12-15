8799b876dba63be866f2731aee180eeab7652547
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-12-15 08:14:15 -0300

    agregue parte del blur a laventana modal

--- js/admin.js
+++ js/admin.js
@@ -4,3 +4,8 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+function toggle () {
+  let blur = document.getElementById('blur');
+  blur.classList.toggle('blur')
+}
