8570e3f79559f95270ae1b4c57c1e468e15e37ba
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-12-17 08:10:30 -0300

    no logro cargar la imagen

---
+++ js/cargarImagenModal.js
@@ -0,0 +1,27 @@
+function nombre_imagen() {
+    const input = document.getElementById('cargarimagen');
+    const nombre_imagen_display = document.getElementById('nombre_imagen');
+    const previewImage = document.getElementById('previewImage');
+  
+    if (input.files.length > 0) {
+      const archivo = input.files[0];
+      const nombre_archivo= archivo.nombre
+    //   nombre de la imagen
+      nombre_imagen_display.textContent = `imagen seleccionada ${nombre_archivo}`;
+
+    //   actualiza laimagen
+      const verimagen = new FileReader();
+      verimagen.onload = function(e) {
+        previewImage.src = e.target.result;
+      };
+      verimagen.readAsDataURL(file);
+
+    } else {
+      nombre_imagen_display.textContent = '';
+      previewImage.src = '';
+    }
+  }
+  
+  
+
+  
