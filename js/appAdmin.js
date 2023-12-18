8d895e2dc037103989668c3180cb85321a6e9f93
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-12-18 11:31:41 -0300

    datos productos logica const

--- js/appAdmin.js
+++ js/appAdmin.js
@@ -12,17 +12,33 @@ const botonAgregarProductoAdmin = document.getElementById('botonAgregarProductoA
 
 const formularioAgregarProductoAdmin = document.getElementById('formAgregarProductoAdmin');
 
+// datos productos
+const nombreProducto = document.getElementById('nombre_prod_cargado');
+const codigo = document.getElementById('codigo_del_producto_card_admin');
+const detalleProducto = document.getElementById('descripcion_del_prod_cargado');
+
+const checkcategoria = document.getElementById('CheckDelProductoAdmin');
+
+const presentacionAdmin = document.getElementById ('presentacionAdmin');
+
+const stockAdmin = document.getElementById('stock_prod_cargado_admin');
+
+const precioProductAdmin = document.getElementById('precio_prod_cargado_admin')
+
+
 
 // funciones
 const mostrarModal = () =>{
     modalAdminProducto.show();
-    console.log('algo')
+   
 
 }
 
 const crearProducto = (e) =>{
-e.preventDefault()
-console.log('creaprodducto');
+e.preventDefault();
+
+
+
 }
 
 
