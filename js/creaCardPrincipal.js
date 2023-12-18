7218b09cbb0053f418e4f79926dc5a89df06b68b
José Vera <profe.jomanuelvr@gmail.com>
2023-12-18 01:53:50 -0300

    los productos simulados se crean con categortias aleatorias, todas las card ahora abren el modal del main

--- js/creaCardPrincipal.js
+++ js/creaCardPrincipal.js
@@ -1,9 +1,23 @@
 // Función para generar una categoría aleatoria de las disponibles
 function generarCategoriasAleatorias() {
     const categorias = [];
+    let tieneCategoria = false;
+
     for (let i = 0; i < 4; i++) {
-        categorias.push(Math.random() < 0.5); // Devuelve true o false aleatoriamente
+        const categoria = Math.random() < 0.5; // Devuelve true o false aleatoriamente
+        categorias.push(categoria);
+
+        if (categoria) {
+            tieneCategoria = true;
+        }
     }
+
+    // Si ninguna categoría está seleccionada, elige una al azar
+    if (!tieneCategoria) {
+        const categoriaAleatoria = Math.floor(Math.random() * 4);
+        categorias[categoriaAleatoria] = true;
+    }
+
     return categorias;
 }
 
@@ -28,9 +42,6 @@ function obtenerCategorias(categorias) {
 
     return categoriasTexto.join(', '); // Devuelve un string con las categorías separadas por coma
 }
-const miCategoria = generarCategoriasAleatorias();
-const textoCategorias = obtenerCategorias(miCategoria);
-console.log(textoCategorias)
 
 // Función para simular la creación de un producto
 function simularCreacionProducto() {
@@ -46,35 +57,29 @@ function simularCreacionProducto() {
     const rutaImagen3 = './img/almendras3.jpg';
     const textoCategorias = obtenerCategorias(catRandom);
 
-
     // Si tienes una función crearProducto y una variable categoriaAleatoria definidas previamente...
-    crearProducto(catRandom, precio, codigo, identificador, descripcion, rutaImagen1, rutaImagen2, rutaImagen3);
-    console.log(crearProducto)
+    crearProducto(catRandom, precio, codigo, identificador, descripcion, rutaImagen1, rutaImagen2, rutaImagen3, textoCategorias);
 }
 
 // Asociar la función simularCreacionProducto a un botón en tu HTML
 const botonSimulacion = document.getElementById('botonSimulacion');
 botonSimulacion.addEventListener('click', simularCreacionProducto);
 
-
-
-function crearProducto(categorias, precio, codigo, identificador, descripcion, rutaImagen1) {
+function crearProducto(categorias, precio, codigo, identificador, descripcion, rutaImagen1, rutaImagen2, rutaImagen3, textoCategorias) {
     const cardProducto = document.createElement('div');
 
-    cardProducto.classList.add('col-lg-3','col-md-4', 'col-6','mt-3', 'mb-5', 'cardProducto');
+    cardProducto.classList.add('col-lg-3', 'col-md-4', 'col-6', 'mt-3', 'mb-5', 'cardProducto');
     cardProducto.innerHTML = `
-        <div class="card">
+        <div class="card" data-bs-toggle="modal" data-bs-target="#modalAllCardsMain">
             <img src="${rutaImagen1}" class="card-img-top img-fluid imgProductos" alt="${descripcion}">
             <div class="card-body">
                 <h5 class="card-title">${descripcion}</h5>
-                <p class="card-text">categorias: ${textoCategorias}</p>
+                <p class="card-text">Categorías: ${textoCategorias}</p>
                 <p class="card-text">$${precio}</p>
-                
             </div>
         </div>
     `;
 
-    
     // Agregar tanto la card como el modal al contenedor 'grillaPrincipal'
     const grillaPrincipal = document.getElementById('grillaPrincipal');
     grillaPrincipal.appendChild(cardProducto);
