58aaa3b51b03e61adda1999d37f8bdcaa0f95070
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-12-18 18:17:24 -0300

    logre cargar el producto local stroge

--- js/appAdmin.js
+++ js/appAdmin.js
@@ -31,21 +31,19 @@ const precioProductAdmin = document.getElementById("precio_prod_cargado_admin");
 
 const imagenProductoAdmin = document.getElementById("cargarimagen");
 
-const productosCargadosAdmin = JSON.parse(localStorage.getItem('productosCargadosAdminKey')) || [];
+const productosCargadosAdmin =
+  JSON.parse(localStorage.getItem("productosCargadosAdminKey")) || [];
 
 // funciones
 const mostrarModal = () => {
   modalAdminProducto.show();
 };
 
-
 // producto
 const crearProducto = (e) => {
   e.preventDefault();
 
-  
-
-//   crea el prodcuto
+  //   crea el prodcuto
   const nuevoProducto = new Producto(
     nombreProducto.value,
     undefined,
@@ -56,57 +54,58 @@ const crearProducto = (e) => {
     precioProductAdmin.value,
     imagenProductoAdmin.value
   );
-  
 
   // array de productos
   productosCargadosAdmin.push(nuevoProducto);
- 
 
   // resetear el formulario
   limpiarformulario();
-// guarda en localstorage
+  // guarda en localstorage
 
-guardarEnLocalStorageAdmin();
-
-
+  guardarEnLocalStorageAdmin();
 };
 
 function limpiarformulario() {
   formularioAgregarProductoAdmin.reset();
-};
-
-function guardarEnLocalStorageAdmin(){
-    localStorage.setItem('productosCargadosAdminKey',JSON.stringify(productosCargadosAdmin))
 }
 
-function crearCardProducto(){
-    const cardProductoAdmin = document.getElementById('cardProductoAdminCrear');
-   cardProductoAdmin.innerHTML = cardProductoAdmin.innerHTML + `<div class="card_producto_admin" >
+function guardarEnLocalStorageAdmin() {
+  localStorage.setItem(
+    "productosCargadosAdminKey",
+    JSON.stringify(productosCargadosAdmin)
+  );
+}
+
+function crearCardProducto(productoAdmin) {
+  const cardProductoAdmin = document.getElementById("cardProductoAdminCrear");
+  cardProductoAdmin.innerHTML =
+    cardProductoAdmin.innerHTML +
+    `<div class="card_producto_admin" >
    <img src="../img/almendras2.png" class="card-img-top" id="card_producto_editar_admin" alt="...">
    <div class="card-body">
-     <h4 class="card-title" id="nombre_del_producto_card_admin">Nombre del producto</h4>
-     <h5 class="card-title" id="codigo_del_producto_card_admin">COD.15526488795</h5>
-     <p class="card-text" id="descripcion_del_producto_admin">Descripcion del producto para desplegar, normalmente dos o tres lineas de texto</p>
+     <h4 class="card-title" id="nombre_del_producto_card_admin">${productoAdmin.nombre}</h4>
+     <h5 class="card-title" id="codigo_del_producto_card_admin">${productoAdmin.cod}</h5>
+     <p class="card-text" id="descripcion_del_producto_admin">${productoAdmin.descripcion}</p>
      <div class="d-flex justify-content-between">
-       <h5 id="categoriaProductoAdmin">categoria</h5>
-       <h5 id="presentacionProductoAdmin">presentacion</h5>
+       <h5 id="categoriaProductoAdmin">${productoAdmin.categoria}</h5>
+       <h5 id="presentacionProductoAdmin">${productoAdmin.presentacion}</h5>
      </div>
      <div class="d-flex justify-content-between align-content-center my-2">
        <h5 class="stock_admin card-title" id="stock_del_producto_admin">
-         STOCK 40kg
+         STOCK ${productoAdmin.stock}
        </h5>
-     <h5 class=" text-end" id="precio_del_prudcto_admin"> PRECIO $2500.25</h5>
+     <h5 class=" text-end" id="precio_del_prudcto_admin"> PRECIO $ ${productoAdmin.precio}</h5>
    </div>
      <a href="#" class="btn btn_editar_admin texto_blanco_btn_amdin">Editar</a>
    </div>
    </div>
-`
+`;
 }
 
-function cargaInicial (){
-    if(productosCargadosAdmin.length > 0){
-        productosCargadosAdmin.map( (productoAdmin)=> crearCardProducto())
-    }
+function cargaInicial() {
+  if (productosCargadosAdmin.length > 0) {
+    productosCargadosAdmin.map((productoAdmin) => crearCardProducto(productoAdmin));
+  }
 }
 
 // logica extra
@@ -114,6 +113,5 @@ function cargaInicial (){
 botonAgregarProductoAdmin.addEventListener("click", mostrarModal);
 
 formularioAgregarProductoAdmin.addEventListener("submit", crearProducto);
-
 
 cargaInicial();
