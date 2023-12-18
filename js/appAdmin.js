3729faef436491051fe434948a957a636619768b
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-12-18 10:51:25 -0300

    funcion boton ventana modal js

--- js/appAdmin.js
+++ js/appAdmin.js
@@ -1,4 +1,23 @@
-import Producto from "./ClassAgregarProducto.js";
+import Producto from "./classAgregarProductoAdmin.js";
 
+
+// cosntantes
 const producto = new Producto ('nueces','COD','ilimitado','son nueces','5000', 'aca va laimagen');
-console.log(producto)
+
+
+
+const modalAdminProducto = new bootstrap.Modal(document.getElementById('adminProducto'));
+
+const botonAgregarProductoAdmin = document.getElementById('botonAgregarProductoAdmin');
+
+// funciones
+const mostrarModal = () =>{
+    modalAdminProducto.show();
+    
+}
+
+
+// logica extra
+
+botonAgregarProductoAdmin.addEventListener('click',mostrarModal);
+
--- js/classAgregarProductoAdmin.js
+++ js/classAgregarProductoAdmin.js
@@ -1,12 +1,12 @@
-class Producto {
+export default class Producto {
   #nombre;
   #cod;
   #stock;
   #descripcion;
   #precio;
-  #image; // New property for the product image
+  #image; 
 
-  constructor(nombre, cod, stock, descripcion, precio, image) {
+  constructor(nombre, cod=1, stock, descripcion, precio, image) {
     this.#nombre = nombre;
     this.#cod = cod;
     this.#stock = stock;
@@ -36,11 +36,11 @@ class Producto {
     return this.#precio;
   }
 
-  getImage() { // Getter for the product image
+  getImage() { 
     return this.#image;
   }
 
-  // Setter methods
+  // Setter 
   setNombre(newNombre) {
     this.#nombre = newNombre;
   }
@@ -61,39 +61,39 @@ class Producto {
     this.#precio = newPrecio;
   }
 
-  setImage(newImage) { // Setter for the product image
+  setImage(newImage) { 
     this.#image = newImage;
   }
 }
 
-// Example usage:
-const producto1 = new Producto(
-  "Product 1",
-  1,
-  10,
-  "Description of Product 1",
-  29.99,
-  "product1.jpg" // Example image file name
-);
+// // Example usage:
+// const producto1 = new Producto(
+//   "Product 1",
+//   1,
+//   10,
+//   "Description of Product 1",
+//   29.99,
+//   "product1.jpg" // Example image file name
+// );
 
-// Accessing private properties and image using getters
-console.log(producto1.getNombre()); // Output: Product 1
-console.log(producto1.getCod());     // Output: 1
-console.log(producto1.getStock());   // Output: 10
-console.log(producto1.getDescripcion());  // Output: Description of Product 1
-console.log(producto1.getPrecio());      // Output: 29.99
-console.log(producto1.getImage());        // Output: product1.jpg
+// // Accessing private properties and image using getters
+// console.log(producto1.getNombre()); // Output: Product 1
+// console.log(producto1.getCod());     // Output: 1
+// console.log(producto1.getStock());   // Output: 10
+// console.log(producto1.getDescripcion());  // Output: Description of Product 1
+// console.log(producto1.getPrecio());      // Output: 29.99
+// console.log(producto1.getImage());        // Output: product1.jpg
 
-// Modifying private properties and image using setters
-producto1.setNombre("Updated Product 1");
-producto1.setCod(2);
-producto1.setStock(15);
-producto1.setDescripcion("Updated description");
-producto1.setPrecio(39.99);
-producto1.setImage("updated_product1.jpg");
-console.log(producto1.getNombre()); // Output: Updated Product 1
-console.log(producto1.getCod());     // Output: 2
-console.log(producto1.getStock());   // Output: 15
-console.log(producto1.getDescripcion());  // Output: Updated description
-console.log(producto1.getPrecio());      // Output: 39.99
-console.log(producto1.getImage());        // Output: updated_product1.jpg
+// // Modifying private properties and image using setters
+// producto1.setNombre("Updated Product 1");
+// producto1.setCod(2);
+// producto1.setStock(15);
+// producto1.setDescripcion("Updated description");
+// producto1.setPrecio(39.99);
+// producto1.setImage("updated_product1.jpg");
+// console.log(producto1.getNombre()); // Output: Updated Product 1
+// console.log(producto1.getCod());     // Output: 2
+// console.log(producto1.getStock());   // Output: 15
+// console.log(producto1.getDescripcion());  // Output: Updated description
+// console.log(producto1.getPrecio());      // Output: 39.99
+// console.log(producto1.getImage());        // Output: updated_product1.jpg
