2398ab10a77bf8716ecdf760bbafd5c0fe7a3bed
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-12-19 02:38:48 -0300

    se realiza la parte de js de login

---
+++ js/jsLogin.js
@@ -0,0 +1,41 @@
+// Variables para almacenar datos simulados
+let usuariosRegistrados =
+  JSON.parse(localStorage.getItem("usuariosRegistrados")) || [];
+
+
+// Función para registrar un nuevo usuario
+function registrarUsuario() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Simulamos almacenar los datos del usuario registrado en localStorage
+  const nuevoUsuario = { email, password };
+  usuariosRegistrados.push(nuevoUsuario);
+  localStorage.setItem(
+    "usuariosRegistrados",
+    JSON.stringify(usuariosRegistrados)
+  );
+
+  alert("Usuario registrado exitosamente");
+
+  // Cerrar la ventana modal después de registrar
+  closeModal("registroModal");
+}
+
+// Función para el login del administrador y redirigir al index
+function loginAdministrador() {
+  const adminEmail = document.getElementById("adminEmail").value;
+  const adminPassword = document.getElementById("adminPassword").value;
+
+  // Verificamos las credenciales del administrador
+  if (
+    adminEmail === "adminderaiz@gmail.com" &&
+    adminPassword === "deraiz1234"
+  ) {
+    alert("Login del administrador exitoso");
+    // Redirigir a la pagina de administracion
+    window.location.href = "../pages/administracion.html";
+  } else {
+    alert("Credenciales incorrectas para el administrador");
+  }
+}
